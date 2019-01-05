087cce08bddb5c81143605a0a65c665c92bd42c4
Antoine POPINEAU <antoine.popineau@appscho.com>
2019-01-05 18:59:56 +0100

    Added tests for commands.

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -12,3 +12,31 @@ pub(crate) fn init(args: &clap::ArgMatches) -> Result<(), Box<dyn Error>> {
 
   Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+  use clap::App;
+
+  use crate::vault::wire;
+
+  #[test]
+  fn init() {
+    let _tmp = crate::tests::setup();
+
+    let yml = load_yaml!("../cli.yml");
+    let app = App::from_yaml(yml).get_matches_from(vec!["", "init", crate::tests::GPG_IDENTITY]);
+
+    if let ("init", Some(args)) = app.subcommand() {
+      assert_eq!(super::init(args).is_ok(), true);
+      assert_eq!(super::init(args).is_err(), true);
+
+      let vault = wire::get_vault().expect("could not get vault metadata");
+
+      assert_eq!(vault.get_identity(), "vault@apognu.github.com");
+
+      return;
+    }
+
+    panic!("command init not triggering");
+  }
+}
