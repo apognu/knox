75585dc5183f00f6fcf77ff30aacc9fe16fb7fd7
Antoine POPINEAU <antoine.popineau@appscho.com>
2019-01-11 10:52:47 +0100

    Adding metadata.

--- vault/build.rs
+++ vault/build.rs
@@ -1,12 +1,13 @@
-use protoc_rust::Customize;
 
 fn main() {
+	use protoc_rust::Customize;
+	
 	protoc_rust::run(protoc_rust::Args {
-	    out_dir: "src/",
-	    input: &["protobufs/pb.proto"],
-	    includes: &["protobufs"],
-	    customize: Customize {
-	      ..Default::default()
-	    },
+		out_dir: "src/",
+		input: &["protobufs/pb.proto"],
+		includes: &["protobufs"],
+		customize: Customize {
+			..Default::default()
+		},
 	}).expect("protoc");
 }
