935a12792a699994f27bee8ceefcf610768c8ab8
Antoine POPINEAU <antoine.popineau@appscho.com>
2019-04-02 22:35:01 +0200

    Remove useless check in publish script [skip ci].

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -14,13 +14,6 @@ for CRATE in ${CRATES[@]}; do
   echo "INFO : processing crate ${CRATE}."
   pushd $CRATE > /dev/null
 
-  VERSION="$(cargo pkgid | cut -d# -f2)"
-
-  if ! curl -s -o /dev/null -w '%{http_code}' https://github.com/apognu/knox/tree/v${VERSION} | grep 200 > /dev/null; then
-    echo "ERROR: tag v${VERSION} does not exist in upstream git repository."
-    exit 1
-  fi
-  
   cp Cargo.toml Cargo.toml.bak
   sed -i 's/build =.*/build = false/' Cargo.toml
   sed -i '/^\[dev-dependencies\]/,/^$/d' Cargo.toml
